ea99602a32da78f5bccf0c17fc053cd636f9a100
kevinweber <kevinweber@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-07-09 19:15:31 +0000

    Added several tags (from 0.8 to 1.0.6)
    git-svn-id: https://plugins.svn.wordpress.org/inline-comments/tags/1.0.6@945770 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ frontend/class-wp.php
@@ -0,0 +1,84 @@
+<?php
+/**
+ * @package Comment System Type: WordPress
+ */
+class INCOM_WordPress extends INCOM_Frontend {
+
+	function __construct() {
+		parent::addActions();
+		$this->addActions();
+	}
+
+	function addActions() {
+		$this->get_comments_php();
+		// add_action( "wp_ajax_get_comments_php", array( $this, 'get_comments_php' ) );
+		// add_action( "wp_ajax_nopriv_get_comments_php", array( $this, 'get_comments_php' ) );
+	}
+
+	function get_comments_php() {
+		require_once( 'class-comments.php' );
+		$comments = new INCOM_Comments();
+		// $comments_php = $comments->getCode();
+		// return $comments_php;
+		// die();
+	}
+
+	/**
+	 * Add Scripts into Footer
+	 */
+	function load_incom() { ?>
+		<script>
+			var $ind = jQuery.noConflict();
+
+			$ind(document).ready(function() {
+				incom.init({
+					selectors: '<?php if (get_option("multiselector") == '') { echo "p"; } else { echo get_option("multiselector"); } ?>',
+					moveSiteSelector: '<?php if (get_option("moveselector") == '') { echo "body"; } else { echo get_option("moveselector"); } ?>',
+	            	countStatic: <?php if (get_option("bubble_static") == "1") { echo "false"; } else { echo "true"; } ?>,
+	            	alwaysStatic: <?php if (get_option("bubble_static_always") == "1") { echo "true"; } else { echo "false"; } ?>,
+	            	bubbleStyle: '<?php if (get_option("select_bubble_style") == "") { echo "bubble"; } else { echo get_option("select_bubble_style"); } ?>',
+	            	bubbleAnimationIn: '<?php if (get_option("select_bubble_fadein") == "") { echo "default"; } else { echo get_option("select_bubble_fadein"); } ?>',
+	            	bubbleAnimationOut: '<?php if (get_option("select_bubble_fadeout") == "") { echo "default"; } else { echo get_option("select_bubble_fadeout"); } ?>',
+				  // defaultBubbleText: '+',
+	              // highlighted: false,
+	            	position: '<?php if (get_option("select_align") == "") { echo "right"; } else { echo get_option("select_align"); } ?>',
+	              	background: '<?php if (get_option("set_bgcolour") == "") { echo "#fff"; } else { echo get_option("set_bgcolour"); } ?>',
+				});
+			});
+		</script>
+	<?php }
+
+	/**
+	 * Add scripts (like JS)
+	 */
+	function incom_enqueue_scripts() {
+		wp_enqueue_script('ajax-script', plugins_url( 'js/min/inline-comments-ck.js' , plugin_dir_path( __FILE__ ) ), array( 'jquery' ) );
+		//wp_localize_script('ajax-script', 'ajax_script_vars', array( 'ajaxurl' => admin_url( 'admin-ajax.php' ), 'comments_php' => $this->get_comments_php() ) );
+	}
+
+	/**
+	 * Add stylesheet
+	 */
+	function load_incom_style() {
+		wp_register_style( 'incom-style', plugins_url('css/min/style-wp.css', plugin_dir_path( __FILE__ ) ) );
+		wp_enqueue_style( 'incom-style' );
+	}
+
+
+	/**
+	 * Add Custom CSS
+	 */
+	function load_incom_custom_css(){
+		echo '<style type="text/css">';
+		if (stripslashes(get_option('custom_css')) != '') {
+			echo stripslashes(get_option('custom_css'));
+		}
+		echo '</style>';
+	}
+
+}
+
+function initialize_incom_wp() {
+	$incom_wp = new INCOM_WordPress();
+}
+add_action( 'init', 'initialize_incom_wp' );
---
+++ functions.php
@@ -0,0 +1,61 @@
+<?php
+/*
+ * Plugin Name: Inline Comments
+ * Plugin URI: http://kevinw.de/inline-comments
+ * Description: Inline Comments adds your comment system to the side of paragraphs, headlines and other sections (like headlines and images) of your post. It performs native with WordPress comments.
+ * Author: Kevin Weber
+ * Version: 1.0.6
+ * Author URI: http://kevinw.de/
+ * License: GPL v3
+ * Text Domain: inline-comments
+*/
+
+if ( !defined( 'INCOM_VERSION' ) ) {
+	define( 'INCOM_VERSION', '1.0.6' );
+}
+
+if ( !defined( 'INCOM_VERSION_NAME' ) ) {
+	define( 'INCOM_VERSION_NAME', 'Essential' );
+}
+
+if ( !defined( 'INCOM_ESSENTIAL' ) ) {
+	define( 'INCOM_ESSENTIAL', true );	// Should be false if this is the 'Lifetime' version
+}
+
+if ( !defined( 'INCOM_FILE' ) ) {
+	define( 'INCOM_FILE', __FILE__ );
+}
+
+if ( !defined( 'INCOM_PATH' ) )
+	define( 'INCOM_PATH', plugin_dir_path( __FILE__ ) );
+
+define( 'INCOM_NEWS_TEXT', 'To suggest and vote for new features: Let the developer come into contact with you.' );
+define( 'INCOM_NEWS_BUTTON', 'Get contacted' );
+
+require_once( INCOM_PATH . 'admin/class-register.php' );
+
+
+function incom_admin_init() {
+	// require_once( INCOM_PATH . 'admin/class-admin.php' );
+	require_once( INCOM_PATH . 'admin/class-admin-options.php' );
+}
+
+function incom_frontend_init() {
+	require_once( INCOM_PATH . 'frontend/class-frontend.php' );
+
+	if ( get_option("select_comment_type") !== "disqus" ) {
+		require_once( INCOM_PATH . 'frontend/class-wp.php' );
+	}
+	else {
+		require_once( INCOM_PATH . 'frontend/class-indisq.php' );
+	}
+}
+
+if ( is_admin() ) {
+	add_action( 'plugins_loaded', 'incom_admin_init', 15 );
+}
+else {
+	add_action( 'plugins_loaded', 'incom_frontend_init', 15 );
+}
+
+/***** Plugin by Kevin Weber || kevinw.de *****/
